5e6017d9a9df5f51b812703e5736d5ff6864adfd
NatalyBMota <138526575+NatalyBMota@users.noreply.github.com>
2023-09-01 18:04:03 -0500

    Added JavaScript code to request more data about meeting room availability through AJAX, and then display it in the HTML of the web-page.

--- js/widget.js
+++ js/widget.js
@@ -1,10 +1,7 @@
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function () {
-  if (xhr.readyState === 4) {
-    //console.log(typeof xhr.responseText);
+  if(xhr.readyState === 4 && xhr.status === 200) {
     var employees = JSON.parse(xhr.responseText);
-    //console.log(typeof employees);
-    //console.log(employees);
     var statusHTML = '<ul class="bulleted">';
     for (var i=0; i<employees.length; i += 1) {
       if (employees[i].inoffice === true) {
@@ -19,5 +16,29 @@ xhr.onreadystatechange = function () {
     document.getElementById('employeeList').innerHTML = statusHTML;
   }
 };
-xhr.open('GET', 'data/employees.json');
+xhr.open('GET', '../data/employees.json');
 xhr.send();
+
+var roomRequest = new XMLHttpRequest();
+roomRequest.onreadystatechange = function () {
+  if (roomRequest.readyState === 4) {
+    //console.log(typeof roomRequest.responseText);
+    var rooms = JSON.parse(roomRequest.responseText);
+    //console.log(typeof rooms);
+    //console.log(rooms);
+    var roomStatusHTML = '<ul class="rooms">';
+    for (var i=0; i<rooms.length; i += 1) {
+      if (rooms[i].available === true) {
+        roomStatusHTML += '<li class="empty">';
+      } else {
+        roomStatusHTML += '<li class="full">';
+      }
+      roomStatusHTML += rooms[i].room;
+      roomStatusHTML += '</li>';
+    }
+    roomStatusHTML += '</ul>';
+    document.getElementById('roomList').innerHTML = roomStatusHTML;
+  }
+};
+roomRequest.open('GET', '../data/rooms.json');
+roomRequest.send();
